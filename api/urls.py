443d56407527b7d577a481b1de5289a7725b9b17
Rohan Dass Gujrati <rohandass58@gmail.com>
2024-01-04 04:49:44 +0530

    done

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductListCreateView, ProductRetrieveUpdateDestroyView, ProductViewSet
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
+router.register(r'products', ProductViewSet, basename='product')
 
 urlpatterns = [
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
     path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,14 @@
 from rest_framework import viewsets
+from rest_framework import generics
+
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductViewSet(viewsets.ModelViewSet):
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
